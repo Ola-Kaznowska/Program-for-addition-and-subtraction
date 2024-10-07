91d8541b3c2206513a7cd59873c521955538fa48
Ola Kaznowska <118020449+Ola-Kaznowska@users.noreply.github.com>
2024-10-07 18:02:37 +0200

    Add files via upload

---
+++ Adding numbers.py
@@ -0,0 +1,15 @@
+import os 
+
+os.system("cls")
+for i in range(100):
+    
+    print("program for adding numbers")
+
+    a = int(input("Enter the first number: "))
+    
+    print()
+
+    b = int(input("Enter the second number: "))
+    print()
+
+    print(a + b)
---
+++ Subtraction of numbers.py
@@ -0,0 +1,15 @@
+import os 
+
+os.system("cls")
+for i in range(100):
+    
+    print("Program na odejmowanie")
+
+    a = int(input("Enter the first number"))
+    print()
+
+    b = int(input("Enter the second number "))
+    
+    print()
+
+    print(a - b)
